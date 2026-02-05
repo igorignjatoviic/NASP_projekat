eaa2666dda17a3876a511180a3a866530f952c3f
Igor Ignjatovic <ignjatovici2006@gmail.com>
2026-02-05 18:25:35 +0100

    wal ispravka zapisa i upis u fajl

--- WriteAheadLog/WriteAheadLog.go
+++ WriteAheadLog/WriteAheadLog.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 // napraviti da sistem sam bira segment iz segmenti.txt i upis writeaheadloga
+// proveriti da li je upis dobar pomocu citanja i zatim napraviti bolji odabir walova, upis u fajl da je zapis promenjen itd
 type WriteAheadLog struct {
-	blok     []byte
-	segmenti []string
+	blok                 []byte
+	segmenti             []string
+	maksimalanBrojZapisa int
 }
 
 func (wal *WriteAheadLog) unesi(dogadjaj, kljuc, vrednost string) {
+	wal.maksimalanBrojZapisa = 10
+
 	wal.izracunajTimestamp()
 	wal.izracunajTombstone(dogadjaj)
 	wal.izracunajDuzinuParametra(kljuc)
 	wal.izracunajDuzinuParametra(vrednost)
+	wal.konvertujParametarUBajtove(kljuc)
+	wal.konvertujParametarUBajtove(vrednost)
 	wal.izracunajCRC()
 
 	wal.sacuvajBlok()
@@ -47,20 +56,55 @@ func (wal *WriteAheadLog) izracunajTombstone(dogadjaj string) {
 func (wal *WriteAheadLog) izracunajDuzinuParametra(parametar string) {
 	duzina := len(parametar)
 	bafer := make([]byte, 8)
-	bafer[0] = byte(duzina)
-	copy(bafer[1:len(parametar)+1], []byte(parametar))
+	binary.BigEndian.PutUint64(bafer, uint64(duzina))
 	wal.blok = append(wal.blok, bafer...)
 }
 
-func (wal *WriteAheadLog) sacuvajBlok() {
-	wal.ucitajSegmente()
+func (wal *WriteAheadLog) konvertujParametarUBajtove(parametar string) {
+	bafer := []byte{}
+	bafer = append(bafer, []byte(parametar)...)
+	wal.blok = append(wal.blok, bafer...)
 }
 
-func (wal *WriteAheadLog) ucitajSegmente() {
-	podaci, err := os.ReadFile("resources/segmenti.txt")
+func (wal *WriteAheadLog) sacuvajBlok() error {
+	segment := wal.ucitajSegmente()
+	fajl, err := os.Create(segment)
 	if err != nil {
-		fmt.Printf("Fajl nepostoji.")
-		return
+		return err
 	}
-	fmt.Println(string(podaci))
+	defer fajl.Close()
+
+	if err := binary.Write(fajl, binary.BigEndian, wal.blok); err != nil {
+		return err
+	}
+	fmt.Println("upisano")
+
+	wal.blok = []byte{}
+	return nil
+}
+
+func (wal *WriteAheadLog) ucitajSegmente() string {
+	fajl, err := os.Open("resources/segmenti.txt")
+	if err != nil {
+		fmt.Println("Greska pri otvaranju fajla.")
+	}
+	defer fajl.Close()
+
+	skener := bufio.NewScanner(fajl)
+	skener.Scan()
+
+	for skener.Scan() {
+		line := skener.Text()
+		podaci := strings.Split(line, ",")
+		brojZapisa, err := strconv.Atoi(podaci[1])
+		if err != nil {
+			fmt.Println("Greska u parsiranju.")
+		}
+
+		if brojZapisa < wal.maksimalanBrojZapisa {
+			return "resources/" + podaci[0] + ".bin"
+		}
+	}
+
+	return ""
 }
